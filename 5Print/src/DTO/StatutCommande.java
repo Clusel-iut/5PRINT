51e292fa163026a949ed8a3110990b05b58f7188
sgour <sgour@LAPTOP-1R3799BT>
2019-01-28 17:29:46 +0100

    update client & create gestion

---
+++ 5Print/src/DTO/StatutCommande.java
@@ -0,0 +1,15 @@
+package DTO;
+
+public enum StatutCommande {
+	En_cours("En cours"), Prêt_à_l_envoi("Prêt à l'envoi"), Envoyée("Envoyée");
+	
+	private String abreviation ;  
+    
+    private StatutCommande(String abreviation) {  
+        this.abreviation = abreviation ;  
+   }
+     
+    public String getAbreviation() {  
+        return  this.abreviation ;  
+   }
+}
