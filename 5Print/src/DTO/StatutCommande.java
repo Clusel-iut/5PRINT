fbf6a02c84552383e3988e8957c3480a9ad4f50c
sebastien GOURAT <s.gourat@laposte.net>
2019-01-30 01:00:21 -0800

    update

--- 5Print/src/DTO/StatutCommande.java
+++ 5Print/src/DTO/StatutCommande.java
@@ -1,7 +1,7 @@
 package DTO;
 
 public enum StatutCommande {
-	En_cours("En cours"), Prêt_à_l_envoi("Prêt à l'envoi"), Envoyée("Envoyée");
+	En_cours("En cours"), Terminée("Terminée"), Prêt_à_l_envoi("Prêt à l'envoi"), Envoyée("Envoyée");
 	
 	private String abreviation ;  
     
